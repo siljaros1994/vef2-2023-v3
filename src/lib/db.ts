85367f4d620e908e1ed6195b091419ec0131764e
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-03-09 22:57:27 +0000

    Create db.ts

---
+++ src/lib/db.ts
@@ -0,0 +1,38 @@
+import { readFile } from 'fs/promises';
+import pg from 'pg';
+
+const { DATABASE_URL: connectionString } = process.env;
+
+const pool = new pg.Pool({ connectionString });
+
+pool.on('error', (err: Error) => {
+  console.error('Villa í tengingu við gagnagrunn, forrit hættir', err);
+  process.exit(-1);
+});
+
+type QueryInput = string | number | null;
+
+export async function query(q: string, values: Array<QueryInput> = []) {
+  let client;
+  try {
+    client = await pool.connect();
+  } catch (e) {
+    console.error('unable to get client from pool', e);
+    return null;
+  }
+
+  try {
+    const result = await client.query(q, values);
+    return result;
+  } catch (e) {
+    console.error('unable to query', e);
+    console.info(q, values);
+    return null;
+  } finally {
+    client.release();
+  }
+}
+
+export async function end() {
+  await pool.end();
+}
