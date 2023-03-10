f3969467fbb5ed1beeed33fb7cf22d0a3ab61790
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-03-10 22:48:40 +0000

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express from 'express';
-import { catchErrors } from './lib/catch-errors.js';
-import { router, bye, hello, error } from './routes/api.js';
+import express, { Request, Response, NextFunction } from 'express';
+import dotenv from 'dotenv';
+import { router } from './routes/api.js';
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', catchErrors(hello), catchErrors(error), catchErrors(bye));
+app.use(express.json());
 app.use(router);
 
 const port = 3000;
@@ -12,3 +14,28 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
+
+function notFoundHandler(req: Request, res: Response, next: NextFunction) {
+  console.warn('Not found', req.originalUrl);
+  res.status(404).json({ error: 'Not found' });
+}
+
+function errorHandler(
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  console.error(err);
+
+  /*
+  if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+    return res.status(400).json({ error: 'Invalid json' });
+  }
+  */
+
+  return res.status(500).json({ error: 'Internal server error' });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
