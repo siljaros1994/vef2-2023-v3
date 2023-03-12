5a3e86f3d6aa79b412d9817b853bb895ff5f5487
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-03-12 22:31:50 +0000

    Update api.ts

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,48 +1,162 @@
-import express, { Request, Response, NextFunction } from 'express';
-import { query } from '../lib/db.js';
-import {
-  mapDbEventsToEvents,
-  eventMapper,
-  mapDbEventToEvent,
-} from '../lib/events.js';
+import express, { Request, Response, NextFunction } from "express";
+import { createDepartment, deleteDepartment, getDepartment, listDepartments, updateDepartment } from "./departments.js";
+import { listCourses, createCourse, getCourse, updateCourse, deleteCourse } from "./courses.js";
+import { Department, Course } from "./types.js";
 
 export const router = express.Router();
 
 export async function index(req: Request, res: Response, next: NextFunction) {
-  const eventsResult = await query('SELECT * FROM events;');
-
-  const events = mapDbEventsToEvents(eventsResult);
-
-  res.json(events);
-}
-
-export async function event(req: Request, res: Response, next: NextFunction) {
-  const { slug } = req.params;
-  const eventsResult = await query('SELECT * FROM events WHERE slug = $1;', [
-    slug,
+  return res.json([
+    {
+      href: "/departments",
+      methods: ["GET", "POST"],
+    },
+    {
+      href: "/departments/:slug",
+      methods: ["GET", "PATCH", "DELETE"],
+    },
+    {
+      href: "/departments/:slug/courses",
+      methods: ["GET", "POST"],
+    },
+    {
+      href: "/departments/:slug/courses/:courseId",
+      methods: ["GET", "PATCH", "DELETE"],
+    },
   ]);
+}
 
-  const event = mapDbEventToEvent(eventsResult);
-
-  if (!event) {
-    return next();
+// Departments
+router.get("/", index); // [200]DONE
+router.get("/departments", async (req: Request, res: Response) => {
+  try {
+    const departments: Department[] = await listDepartments();
+    return res.status(200).json(departments);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
   }
+});
 
-  res.json(event);
-}
-
-async function patchEvent() {}
-
-async function createEvent(req: Request, res: Response, next: NextFunction) {
+router.post("/departments", async (req: Request, res: Response) => {
   const { title, slug, description } = req.body;
+  if (!title || !slug || !description) {
+    return res.status(400).send("Missing required fields");
+  }
+  try {
+    const newDepartment: Department = await createDepartment(title, slug, description);
+    return res.status(200).json(newDepartment);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
-  res.json({ title, slug, description });
-}
+router.get("/departments/:slug", async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  try {
+    const department: Department = await getDepartment(slug);
+    if (!department) {
+      return res.status(404).send("Department not found");
+    }
+    return res.status(200).json(department);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
-async function deleteEvent() {}
+router.patch("/departments/:slug", async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  const { title, description } = req.body;
+  if (!title || !description) {
+    return res.status(400).send("Missing required fields");
+  }
+  try {
+    const updatedDepartment: Department = await updateDepartment(slug, title, description);
+    if (!updatedDepartment) {
+      return res.status(404).send("Department not found");
+    }
+    return res.status(200).json(updatedDepartment);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
 
-router.get('/', index);
-router.get('/:slug', event);
-router.patch('/:slug', patchEvent);
-router.post('/', createEvent);
-router.delete('/:slug', deleteEvent);
+router.delete("/departments/:slug", async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  try {
+    const deletedDepartment: Department = await deleteDepartment(slug);
+    if (!deletedDepartment) {
+      return res.status(404).send("Department not found");
+    }
+    return res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal ServerError");
+  }
+});
+
+router.post("/departments/:slug/courses", async (req: Request, res: Response) => {
+  const { slug } = req.params;
+  const { title, description } = req.body;
+  if (!title || !description) {
+    return res.status(400).send("Missing required fields");
+  }
+  try {
+    const newCourse: Course = await createCourse(slug, title, description);
+    return res.status(200).json(newCourse);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.get("/departments/:slug/courses/:courseId", async (req: Request, res: Response) => {
+  const { slug, courseId } = req.params;
+  try {
+    const course: Course = await getCourse(slug, courseId);
+    if (!course) {
+      return res.status(404).send("Course not found");
+    }
+    return res.status(200).json(course);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.patch("/departments/:slug/courses/:courseId", async (req: Request, res: Response) => {
+  const { slug, courseId } = req.params;
+  const { title, description } = req.body;
+  if (!title || !description) {
+    return res.status(400).send("Missing required fields");
+  }
+  try {
+    const updatedCourse: Course = await updateCourse(slug, courseId, title, description);
+    if (!updatedCourse) {
+      return res.status(404).send("Course not found");
+    }
+    return res.status(200).json(updatedCourse);
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.delete("/departments/:slug/courses/:courseId", async (req: Request, res: Response) => {
+  const { slug, courseId } = req.params;
+  try {
+    const deletedCourse: Course = await deleteCourse(slug, courseId);
+    if (!deletedCourse) {
+      return res.status(404).send("Course not found");
+    }
+    return res.status(204).send();
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+export default router;
