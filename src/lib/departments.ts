c5860ce061531f339f3bd7d0534a5c5471b9b821
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-03-13 00:40:57 +0000

    Update departments.ts

--- src/lib/departments.ts
+++ src/lib/departments.ts
@@ -19,12 +19,61 @@ export class DepartmentsDAO {
     }
   }
 
+  async createDepartment(department: Department): Promise<Department | null> {
+    const client = await this.pool.connect();
+    try {
+      const result = await client.query(
+        'INSERT INTO departments (title, slug, description, created, updated) VALUES ($1, $2, $3, $4, $5) RETURNING *',
+        [department.title, department.slug, department.description, department.created, department.updated]
+      );
+      const createdDepartment = departmentMapper(result.rows[0]);
+      return createdDepartment;
+    } finally {
+      client.release();
+    }
+  }
+
   async getAllDepartments(): Promise<Department[]> {
     const client = await this.pool.connect();
     try {
       const result = await client.query('SELECT * FROM departments');
       const departments = departmentsMapper(result);
       return departments;
+    } finally {
+      client.release();
+    }
+  }
+
+  async deleteDepartment(id: number): Promise<void> {
+    const client = await this.pool.connect();
+    try {
+      await client.query('DELETE FROM departments WHERE id = $1', [id]);
+    } finally {
+      client.release();
+    }
+  }
+
+  async updateDepartment(id: number, updates: Partial<Department>): Promise<Department | null> {
+    const client = await this.pool.connect();
+    try {
+      const existingDepartment = await this.getDepartmentById(id);
+      if (!existingDepartment) {
+        return null;
+      }
+
+      const updatedDepartment: Department = {
+        ...existingDepartment,
+        ...updates,
+        updated: new Date(),
+      };
+
+      const result = await client.query(
+        'UPDATE departments SET title = $1, slug = $2, description = $3, updated = $4 WHERE id = $5 RETURNING *',
+        [updatedDepartment.title, updatedDepartment.slug, updatedDepartment.description, updatedDepartment.updated, id]
+      );
+
+      const department = departmentMapper(result.rows[0]);
+      return department;
     } finally {
       client.release();
     }
