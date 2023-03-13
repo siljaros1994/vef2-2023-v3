ea8a61d9492b98ccdc70eff8ed6c092ea5a3302a
siljaros1994 <70152073+siljaros1994@users.noreply.github.com>
2023-03-13 00:41:20 +0000

    Create Department.test.ts

---
+++ src/test/Department.test.ts
@@ -0,0 +1,60 @@
+import { Pool } from 'pg';
+import { DepartmentsDAO } from '../src/departments.ts';
+
+describe('DepartmentsDAO', () => {
+  let dao: DepartmentsDAO;
+  let pool: Pool;
+
+  beforeAll(() => {
+    pool = new Pool({
+      connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/testdb',
+      ssl: process.env.NODE_ENV === 'production',
+    });
+    dao = new DepartmentsDAO(pool);
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  it('should retrieve department by id', async () => {
+    const department = await dao.getDepartmentById(1);
+    expect(department).not.toBeNull();
+    expect(department?.title).toBe('Computer Science');
+  });
+
+  it('should retrieve all departments', async () => {
+    const departments = await dao.getAllDepartments();
+    expect(departments).toHaveLength(2);
+  });
+
+  it('should create a new department', async () => {
+    const newDepartment = {
+      title: 'Electrical Engineering',
+      slug: 'electrical-engineering',
+      description: 'The study of electrical systems and technology',
+      created: new Date(),
+      updated: new Date(),
+    };
+    const createdDepartment = await dao.createDepartment(newDepartment);
+    expect(createdDepartment).not.toBeNull();
+    expect(createdDepartment?.title).toBe('Electrical Engineering');
+  });
+
+  it('should update an existing department', async () => {
+    const updates = {
+      title: 'Computer Science and Engineering',
+      description: 'The study of computing and engineering',
+    };
+    const updatedDepartment = await dao.updateDepartment(1, updates);
+    expect(updatedDepartment).not.toBeNull();
+    expect(updatedDepartment?.title).toBe('Computer Science and Engineering');
+    expect(updatedDepartment?.description).toBe('The study of computing and engineering');
+  });
+
+  it('should delete an existing department', async () => {
+    await dao.deleteDepartment(2);
+    const departments = await dao.getAllDepartments();
+    expect(departments).toHaveLength(1);
+  });
+});
